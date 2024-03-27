baffcb8ad57632eb6a3ab8307f4133ca9f9f2f71
Pierre Sourice <pierre.sourice38@gmail.com>
2024-03-27 12:36:02 +0100

    Ajouter les fichiers SCSS pour la page d'accueil, l'en-tête, le pied de page et la bannière

--- P8_location_immobiliere_kasa_react/src/components/Banner.jsx
+++ P8_location_immobiliere_kasa_react/src/components/Banner.jsx
@@ -1,10 +1,15 @@
+import image from "../assets/banners/banner-home.png";
+
 export default function Banner() {
 	return (
 		<div className="banner">
-			<div className="banner-text">
-				<h1>Des maisons et appartements</h1>
-				<h2>à louer</h2>
-			</div>
+			<img
+				className="banner-image"
+				src={image}
+				alt="banner"
+			/>
+			<div className="banner-background"></div>
+			<div className="banner-title">Chez vous, partout et ailleurs</div>
 		</div>
 	);
 }
