eedea1e9843aee3fd46f92a0699dcd1e8a02f884
Pierre Sourice <pierre.sourice38@gmail.com>
2024-04-19 16:45:51 +0200

    Ajouter le type de propriété manquant dans le composant Rate

--- P8_location_immobiliere_kasa_react/src/components/Rate.jsx
+++ P8_location_immobiliere_kasa_react/src/components/Rate.jsx
@@ -1,7 +1,8 @@
 import StarActive from "../assets/stars/star-active.png";
 import StarInactive from "../assets/stars/star-inactive.png";
+import PropTypes from "prop-types";
 
-export default function Rate(rating) {
+export default function Rate({ rating }) {
 	const range = [1, 2, 3, 4, 5];
 	const countRating = parseInt(rating);
 
@@ -27,3 +28,7 @@ export default function Rate(rating) {
 		</div>
 	);
 }
+
+Rate.PropTypes = {
+	rating: PropTypes.number
+};
